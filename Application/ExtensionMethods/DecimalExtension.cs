65cf0d84e51a5db2549bfaa3218ff9e45247fe40
Ramprasath Babu <ramprasath.technology@gmail.com>
2021-10-09 23:38:17 -0500

    Simplify decimal rounding

---
+++ Application/ExtensionMethods/DecimalExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.ExtensionMethods
+{
+    internal static class DecimalExtension
+    {
+        static int roundingFactor = 2;
+        public static decimal RoundDecimal(this decimal input)
+        {
+            return decimal.Round(input, roundingFactor, MidpointRounding.AwayFromZero);
+        }
+    }
+}
